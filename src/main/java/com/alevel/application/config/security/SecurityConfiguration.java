c1319b64d658cea97fd983dfe63e98f2a1be17d2
Yaroslav <yaroslavsid05@gmail.ru>
2021-07-22 08:10:12 +0300

    change security configuration

--- src/main/java/com/alevel/application/config/security/SecurityConfiguration.java
+++ src/main/java/com/alevel/application/config/security/SecurityConfiguration.java
@@ -18,6 +18,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsService adminDetailsServiceImpl;
 
+    private static final String[] AUTH_SWAGGER_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -26,9 +39,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+       http
+               .httpBasic()
+                .disable()
+                .csrf()
+                .disable()
+                .authorizeRequests()
                 .antMatchers("/bot").permitAll()
-                .antMatchers("/swagger-ui/index.html").permitAll()
+                .antMatchers(AUTH_SWAGGER_WHITELIST).permitAll()
                 .anyRequest().hasAuthority("ADMIN")
                 .and()
                 .formLogin().defaultSuccessUrl("/drugs", true).permitAll();
